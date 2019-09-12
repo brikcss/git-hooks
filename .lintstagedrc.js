5e5d2387f2354f7e5a6a52cb883dd63ce684c047
Zimmee <zimmee@ross.localdomain>
2019-09-12 12:09:53 -0600

    tools: Update dependencies.
    BREAKING: Lint staged no longer supports advanced configuration (see .lintstagedrc.js).
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # Committer: Zimmee <zimmee@ross.localdomain>
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   .lintstagedrc.js
    #	modified:   package-lock.json
    #	modified:   package.json
    #
    
    [BRANCH: master]

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,17 +1,11 @@
 /*! .lintstagedrc.js | @author brikcss <https://github.com/brikcss> | @reference <https://github.com/okonet/lint-staged> */
 
 module.exports = {
-  linters: {
-    '*.js': ['standard --fix', 'git add'],
-    '*.json': ['prettier --parser json --write', 'git add'],
-    '*.{yml,yaml}': ['prettier --parser yaml --write', 'git add'],
-    '*.md': ['prettier --parser markdown --write', 'git add'],
-    '*.html': ['prettier --parser html --write', 'git add']
-  },
-  concurrent: true,
-  globOptions: {
-    matchBase: true,
-    dot: true
-  },
-  ignore: ['*.min.{js,css}']
+  '!(*.min).js': ['standard --fix', 'git add'],
+  '!(*.min).css': ['prettier --parser css --write', 'git add'],
+  '*.scss': ['prettier --parser scss --write', 'git add'],
+  '*.json': ['prettier --parser json --write', 'git add'],
+  '*.{yml,yaml}': ['prettier --parser yaml --write', 'git add'],
+  '*.md': ['prettier --parser markdown --write', 'git add'],
+  '*.html': ['prettier --parser html --write', 'git add']
 }
